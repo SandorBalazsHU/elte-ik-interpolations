02709261384fe5552a8edf691e9d19ebde6eb09b
Sándor Balázs <it.sandor.balazs@gmail.com>
2020-09-30 09:59:32 +0200

    Updated input field

--- nummod.js
+++ nummod.js
@@ -1,6 +1,15 @@
 const calculate = document.querySelector("#calculate");
 const inputField = document.querySelector("#inputField");
 const outputField = document.querySelector("#outputField");
+var datas = {"x":[-2,-1,0,1,2],
+"y":[
+[-15,0,0,0,0],
+[-4, 0,0,0,0],
+[-1, 0,0,0,0],
+[ 0, 0,0,0,0],
+[ 5, 0,0,0,0]]
+};
+var numberOfDatas = 5;
 
 calculate.addEventListener("pointerdown", function() {
   interpolation();
@@ -14,21 +23,101 @@ function dividedDiffTable(x, y, n) {
 	} 
 }
 
-function createTable(tableDiv, tableData) {
+function createInputTable(tableDiv, tableData, numberOfDatas) {
   var table = document.createElement('table');
   var tableBody = document.createElement('tbody');
 
-  tableData.forEach(function(rowData) {
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.appendChild(document.createTextNode("x:"));
+  row.appendChild(cell);
+
+  for (let i = 0; i < numberOfDatas; i++) {
+    var cell = document.createElement('td');
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'inputX'+i);
+    input.setAttribute('size', '2');
+    input.setAttribute('value', tableData.x[i]);
+    cell.appendChild(input);
+    row.appendChild(cell);
+  }
+
+  cell = document.createElement('td');
+  var input = document.createElement("input");
+  input.setAttribute('type', 'button');
+  input.setAttribute('value', "+");
+  cell.appendChild(input);
+  row.appendChild(cell);
+
+  tableBody.appendChild(row);
+
+
+
+
+
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.appendChild(document.createTextNode("y:"));
+  row.appendChild(cell);
+
+  for (let i = 0; i < numberOfDatas; i++) {
+    var cell = document.createElement('td');
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'inputY'+i);
+    input.setAttribute('size', '2');
+    input.setAttribute('value', tableData.y[i][0]);
+    cell.appendChild(input);
+    row.appendChild(cell);
+  }
+
+  tableBody.appendChild(row);
+
+
+
+
+  table.appendChild(tableBody);
+  tableDiv.appendChild(table);
+}
+createInputTable(inputField, datas, numberOfDatas);
+
+
+
+
+function updateDatasFromInputField(inputField, datas, numberOfDatas) {
+  for (let i = 0; i < numberOfDatas; i++) {
+    var cell = document.querySelector('#inputX'+i);
+    datas.x[i] = cell.value;
+    cell = document.querySelector('#inputY'+i);
+    datas.y[i][0] = cell.value;
+  }
+}
+
+
+
+
+
+function createTable(tableDiv, tableData, numberOfDatas) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+
+  for (var i = 0; i < numberOfDatas; i++) {
+
     var row = document.createElement('tr');
 
-    rowData.forEach(function(cellData) {
+    var cell = document.createElement('td');
+    cell.appendChild(document.createTextNode(tableData.x[i]));
+    row.appendChild(cell);
+
+    tableData.y[i].forEach(function(cellData) {
       var cell = document.createElement('td');
       cell.appendChild(document.createTextNode(cellData));
       row.appendChild(cell);
     });
 
     tableBody.appendChild(row);
-  });
+  };
 
   table.appendChild(tableBody);
   tableDiv.appendChild(table);
@@ -36,7 +125,7 @@ function createTable(tableDiv, tableData) {
 
 function interpolation() {
   outputField.innerHTML = "";
-  var datas = JSON.parse(inputField.value);
+  updateDatasFromInputField(inputField, datas, numberOfDatas);
 
   dividedDiffTable(datas.x, datas.y, datas.x.length);
 
@@ -55,9 +144,6 @@ function interpolation() {
     if (i < n-1) Lx += " + ";
   }
 
-  for (var i = 0; i < datas.x.length; i++) {
-    datas.y[i].unshift(datas.x[i]);
-  }
-  createTable(outputField,datas.y);
+  createTable(outputField, datas, numberOfDatas);
   outputField.innerHTML += "<br> Lx = " + Lx;
 }
